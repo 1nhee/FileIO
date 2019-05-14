3f96fe2f97e85ff550c7b98214c7a5d8656389eb
1nhee <21800025@handong.edu>
2019-05-14 15:31:25 +0900

    lab

---
+++ src/main/java/edu/handong/csee/java/example/binarydemo/NumbersDoubledReader.java
@@ -0,0 +1,28 @@
+package edu.handong.csee.java.example.binarydemo;
+
+import java.io.EOFException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+public class NumbersDoubledReader {
+	String fileName = "numbersDoubled.dat";
+	
+	try {
+		ObjectInputStream inputStream = new ObjectInputStream(
+							new FileInputStream(fileName));
+		System.out.println("in the file " + fileName);
+		while(true) {
+			int anInteger = inputStream.readInt();
+			System.out.println(anInteger);
+		}
+	} catch(FileNotFoundException e) {
+		System.out.println("Problem opening the file " + fileName);
+	} catch (EOFException e) {
+		System.out.println("Reached end of the file.");
+	} catch (IOException e) {
+		System.out.println("Problem reading the file " + fileName);
+	}
+}
+}
